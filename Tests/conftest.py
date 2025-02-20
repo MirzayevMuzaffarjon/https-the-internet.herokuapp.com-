4649b82f88335e9df1a0333b4075e4b5b701c181
MirzayevMuzaffarjon <mirzayevmuzaffar525@gmail.com>
2025-02-20 20:06:35 +0500

    headless=True

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=100)
+        browser = p.chromium.launch(headless=True, slow_mo=100)
         context = browser.new_context(viewport={"width": 1500, "height": 700})
         page = context.new_page()
         yield page
